40d5ff520b3f6d479620abee89eb5b098bee324c
66fiona66 <a24fiomongia@inspedralbes.cat>
2025-12-18 10:59:08 +0100

    Commit: actualització carpetes

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
+
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
+
 let database = null;
 
 async function connectDB() {
