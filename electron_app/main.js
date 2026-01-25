b1c47df7afe112c8d46f571417740c6a19752b55
FabrizzioRodre <a20frarodgon@inspedralbes.cat>
2026-01-25 19:36:55 +0100

    Electron listo

--- electron_app/main.js
+++ electron_app/main.js
@@ -12,7 +12,12 @@ function createWindow() {
     }
   });
 
-  win.loadFile(path.join(__dirname, 'dist/index.html'));
+  const startUrl = process.env.ELECTRON_START_URL || 'http://localhost:5173';
+  if (process.env.NODE_ENV === 'development' || process.env.ELECTRON_START_URL) {
+    win.loadURL(startUrl);
+  } else {
+    win.loadFile(path.join(__dirname, 'dist/index.html'));
+  }
 }
 
 app.whenReady().then(createWindow);
